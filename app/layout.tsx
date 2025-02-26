dcecf19cf5444e660ef5fa4ed19ca87efdadcac4
Maximus Powers <maximusp@uplinq.com>
2025-02-25 20:01:19 -0700

    feat: icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Remembery",
+  title: "reMind",
   description: "A daily podcast/curriculum made from YOUR notes.",
 };
 
